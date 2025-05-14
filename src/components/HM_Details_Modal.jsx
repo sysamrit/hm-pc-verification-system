f80a812ede6d5196190def60cb5b80b8a2cd2456
moumitadas28 <system@amritgroups.in>
2025-05-14 14:24:08 +0530

    hm_pc_verification_v1

---
+++ src/components/HM_Details_Modal.jsx
@@ -0,0 +1,214 @@
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
+import "../style/Modal.css";
+import { Commet } from "react-loading-indicators";
+
+function HM_Details_Modal({ toggleContainerHmPc, hm_pc_id, hm_pc_data }) {
+    console.log(hm_pc_id);
+    const [formData, setFormData] = useState({});
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setFormData(hm_pc_data || {});
+    }, [hm_pc_data]);
+
+    // Handle input change
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
+    const clickAadhaar = () => {
+        if (formData.hm_aadhaar_card_pic) {
+            window.open(formData.hm_aadhaar_card_pic, "_blank");
+        } else {
+            alert("No Aadhaar Card photo available.");
+        }
+    }
+
+    const clickBankPassbook = () => {
+        if (formData.hm_bank_pass_pic) {
+            window.open(formData.hm_bank_pass_pic, "_blank");
+        } else {
+            alert("No Passbook photo available.");
+        }
+    }
+
+    const clickVerified = async () => {
+        const remarks = prompt("Please Enter Remarks");
+        if (remarks == null || remarks.trim() == "") {
+            alert("Remarks is required");
+            return;
+        }
+        setLoading(true);
+        const timestamp = new Date();
+        const formatted_date = timestamp.getFullYear() + "-" + (timestamp.getMonth() + 1) + "-" + timestamp.getDate() + " " + timestamp.getHours() + ":" + timestamp.getMinutes() + ":" + timestamp.getSeconds();
+        const completeData = {
+            // ...formData,
+            timestamp: formatted_date,
+            hm_pc_id: hm_pc_id,
+            remarks:remarks,
+        };
+        await sendRemarks(completeData, "Verified");
+        setLoading(false);
+        console.log(completeData);
+        toggleContainerHmPc();
+        window.location.reload();
+    }
+
+    const clickReject = async() => {
+        const remarks = prompt("Please Enter Remarks");
+        if (remarks == null || remarks.trim() == "") {
+            alert("Remarks is required");
+            return;
+        }
+        setLoading(true);
+        const timestamp = new Date();
+        const formatted_date = timestamp.getFullYear() + "-" + (timestamp.getMonth() + 1) + "-" + timestamp.getDate() + " " + timestamp.getHours() + ":" + timestamp.getMinutes() + ":" + timestamp.getSeconds();
+        const completeData = {
+            // ...formData,
+            timestamp: formatted_date,
+            hm_pc_id: hm_pc_id,
+            remarks:remarks,
+        };
+        await sendRemarks(completeData, "Rejected");
+        setLoading(false);
+        console.log(completeData);
+        toggleContainerHmPc();
+        window.location.reload();
+    }
+
+    const sendRemarks = async (responseData, status) => {
+        const payload = {
+            responseData,
+            action: status, // Verified or Reject
+        };
+        console.log(payload);
+
+        try {
+            const response = await fetch(
+                `${process.env.REACT_APP_BASE_URL2}/hmpc/sethmpccrmver`,
+                {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(payload),
+                }
+            );
+
+            const result = await response.json();
+            console.log("Success:", result);
+            alert(result.message);
+
+        } catch (error) {
+            console.error("Error sending data:", error);
+            alert("An error occurred while sending data. Please try again.");
+            setLoading(false);
+
+        }
+    };
+
+    const clickClose = () => {
+        toggleContainerHmPc();
+        // window.location.reload();
+    }
+
+    return (hm_pc_data &&
+        <>
+            <div className='view_container'>
+                <div>
+                    <h4 className="heading_contant">HM Details</h4>
+                    <hr className="header-line" />
+                </div>
+                <div className='sub_div'>
+                    <div className='lead_details_container'>
+                        {loading && (
+                            <div className="loading-indicator-container">
+                                <Commet color="red" size="medium" text="" textColor="" />
+                            </div>
+                        )}
+                        <div className="fields">
+                            <label>Registration Number:</label>
+                            <input type='text' name='registration_no' id='registration_no' value={formData.registration_no || ""} onChange={handleInputChange} readOnly />
+                        </div>
+                        <div className="fields">
+                            <label>IHB Name:</label>
+                            <input type='text' name='ihb_name' id='ihb_name' value={formData.ihb_name || ""} onChange={handleInputChange} readOnly />
+                        </div>
+                        <div className="fields">
+                            <label>Executive Name</label>
+                            <input type="text" name="executive_name" id="executive_name" value={formData.executive_name || ""} onChange={handleInputChange} readOnly />
+                        </div>
+                        <div className="fields">
+                            <label>HM Name</label>
+                            <input type="text" name="hm_name" id="hm_name" value={formData.hm_name || ""} onChange={handleInputChange} readOnly />
+                        </div>
+                        <div className="fields">
+                            <label>HM Mobile Number</label>
+                            <input type="number" name="hm_mobile_no" id="hm_mobile_no" value={formData.hm_mobile_no || ""} onChange={handleInputChange} readOnly />
+                        </div>
+                        <div className="fields">
+                            <label>HM Aadhaar Card Number</label>
+                            <input type="number" name="hm_aadhaar_card_no" id="hm_aadhaar_card_no" value={formData.hm_aadhaar_card_no || ""} onChange={handleInputChange} readOnly />
+                        </div>
+                        <div className="view_btn_container">
+                            <button className="view-btn" onClick={clickAadhaar}>View Aadhaar Card Photo </button>
+                        </div>
+                        <div className="view_btn_container">
+                            <button className="view-btn" onClick={clickBankPassbook}>View Bank Passbook Photo </button>
+                        </div>
+                        <div className="fields">
+                            <label>HM Bank Account Number</label>
+                            <input type="number" name="hm_bank_account_no" id="hm_bank_account_no" value={formData.hm_bank_account_no || ""} onChange={handleInputChange} readOnly />
+                        </div>
+                        <div className="fields">
+                            <label>HM Bank Name</label>
+                            <input type="text" name="hm_bank_name" id="hm_bank_name" value={formData.hm_bank_name || ""} onChange={handleInputChange} readOnly />
+                        </div>
+                        <div className="fields">
+                            <label>HM Branch Name</label>
+                            <input type="text" name="hm_branch_name" id="hm_branch_name" value={formData.hm_branch_name || ""} onChange={handleInputChange} readOnly />
+                        </div>
+                        <div className="fields">
+                            <label>HM IFSC Code</label>
+                            <input type="text" name="hm_ifsc_code" id="hm_ifsc_code" value={formData.hm_ifsc_code || ""} onChange={handleInputChange} readOnly />
+                        </div>
+             
+                        <div className="fields">
+                            <label>HM DOB</label>
+                            <input type="number" name="dob_of_hm" id="dob_of_hm" value={formData.dob_of_hm || ""} onChange={handleInputChange} readOnly />
+                        </div>
+
+
+                    </div>
+
+
+                    <div className='button-container'>
+                        <button className="accept_btn" type='button' onClick={clickVerified}>
+                            Verified
+                        </button>
+
+                        <button className="reject_btn" type='button' onClick={clickReject}>
+                            Rejected
+                        </button>
+
+                        <button className="close_btn"
+                            type='button'
+                            onClick={clickClose}
+                        >
+                            Close
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    );
+
+}
+
+export default HM_Details_Modal
---
+++ src/pages/CrmDashboard.jsx
@@ -0,0 +1,218 @@
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+// import 'ag-grid-community/styles/ag-theme-quartz.css';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+import "../style/crm_dashboard.css";
+import logo_img from "../assets/LOGO.png";
+import axios from 'axios';
+import HM_PC_Modal from '../components/HM_PC_Modal';
+import PC_Details_Modal from '../components/PC_Details_Modal';
+import HM_Details_Modal from '../components/HM_Details_Modal';
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+function CrmDashboard() {
+    const gridRef = useRef();
+    const [rowData, setRowData] = useState(null);
+    const [isVisibleModal, setIsVisibleModal] = useState(false);
+    const [hm_pc_id, setHm_pc_id] = useState("");
+    const [hmpcData, setHmpcData] = useState();
+    const [kycDetails, setKycDetails] = useState("");
+
+    const handleActionClick = (rowData) => {
+        setIsVisibleModal(true);
+        const hm_pc_id = rowData.hm_pc_id;
+        console.log("HM-PC ID:", hm_pc_id);
+        setKycDetails(rowData.kyc_details);
+        setHm_pc_id(hm_pc_id);
+        fetchHm_PcData(hm_pc_id);
+    };
+    const hideModal = () => {
+        setIsVisibleModal(false);
+    }
+
+
+    const fetchHm_PcData = async (hm_pc_id) => {
+        console.log(hm_pc_id)
+        try {
+            const response = await axios.get(
+                `${process.env.REACT_APP_BASE_URL2}/hmpc/gethmpcdatabyid/${hm_pc_id}`
+            );
+            console.log(response.data.data[0]);
+            setHmpcData(response.data.data[0]);
+        } catch (error) {
+            console.error("Error fetching HM-PC data:", error);
+        }
+    };
+
+    // useEffect(() => {
+    //     axios.get(`${process.env.REACT_APP_BASE_URL2}/hmpccrm/gethmpccrmdatabyid/${hm_pc_id}`)
+    //         .then((res) => {
+    //             console.log(res.data.data);
+    //             setFormData(res.data.data[0]);
+    //         })
+    //         .catch((err) => console.error(err));
+    // }, [hm_pc_id]);
+
+
+    const columnDefs = [
+        {
+            headerName: "TimeStamp",
+            field: "timestamp",
+            width: 250
+            // flex: 2  
+        },
+        { headerName: "Registration No.", field: "registration_no" },
+        { headerName: "IHB Name", field: "ihb_name" },
+        { headerName: "Executive Name", field: "executive_name" },
+        { headerName: "KYC Details", field: "kyc_details" },
+        { headerName: "HM Name", field: "hm_name" },
+        { headerName: "HM Mobile Number", field: "hm_mobile_no" },
+        { headerName: "HM Aadhaar Card Number", field: "hm_aadhaar_card_no" },
+        // { headerName: "HM Aadhaar Card Picture", field: "hm_aadhaar_card_pic" },
+        {
+            headerName: "HM Aadhaar Card Picture",
+            field: "hm_aadhaar_card_pic",
+            cellRenderer: params => (
+                <a href={params.value} target="_blank" rel="noopener noreferrer">View</a>
+            )
+        },
+        { headerName: "HM Bank Account Number", field: "hm_bank_account_no" },
+        // { headerName: "HM Bank Passbook Picture", field: "hm_bank_pass_pic" },
+        {
+            headerName: "HM Bank Passbook Picture",
+            field: "hm_bank_pass_pic",
+            cellRenderer: params => (
+                <a href={params.value} target="_blank" rel="noopener noreferrer">View</a>
+            )
+        },
+        { headerName: "DOB of HM", field: "dob_of_hm" },
+        { headerName: "PC Name", field: "pc_name" },
+
+        { headerName: "PC Mobile Number", field: "pc_mobile_no" },
+        { headerName: "PC Aadhaar Card Number", field: "pc_aadhaar_card_no" },
+        // { headerName: "PC Aadhaar Card Picture", field: "pc_aadhaar_card_pic" },
+        {
+            headerName: "PC Aadhaar Card Picture",
+            field: "pc_aadhaar_card_pic",
+            cellRenderer: params => (
+                <a href={params.value} target="_blank" rel="noopener noreferrer">View</a>
+            )
+        },
+        { headerName: "PC Bank Account Number", field: "pc_bank_account_no" },
+        // { headerName: "PC bank Passbook Picture", field: "pc_bank_pass_pic" },
+        {
+            headerName: "PC Bank Passbook Picture",
+            field: "pc_bank_pass_pic",
+            cellRenderer: params => (
+                <a href={params.value} target="_blank" rel="noopener noreferrer">View</a>
+            )
+        },
+        { headerName: "DOB of PC", field: "dob_of_pc" },
+        { headerName: "Code of HM", field: "code_of_hm" },
+        { headerName: "Code of PC", field: "code_of_pc" },
+        { headerName: "Status", field: "crm_ver" },
+        { headerName: "Verification Date", field: "verify_date" },
+        { headerName: "HM Bank Name", field: "hm_bank_name" },
+        { headerName: "HM Branch Name", field: "hm_branch_name" },
+        { headerName: "HM IFSC Code", field: "hm_ifsc_code" },
+        { headerName: "PC Bank Name", field: "pc_bank_name" },
+        { headerName: "PC Branch Name", field: "pc_branch_name" },
+        { headerName: "PC IFSC Code", field: "pc_ifsc_code" },
+        { headerName: "Remarks", field: "remarks_crm" },
+        {
+            headerName: "Action",
+            field: "action",
+            cellRenderer: (params) => {
+                const isDisabled = params.data.crm_ver === "Verified" || params.data.crm_ver === "Rejected";
+        
+                return (
+                    <button
+                        onClick={() => handleActionClick(params.data)}
+                        className='action_btn'
+                        disabled={isDisabled}
+                    >
+                        Action
+                    </button>
+                );
+            }
+        }
+        
+    ];
+
+    const defaultColDef = useMemo(() => ({
+        sortable: true,
+        filter: true,
+        floatingFilter: true,
+        resizable: true,
+        editable: true,
+    }), []);
+
+    useEffect(() => {
+        axios
+            .get(`${process.env.REACT_APP_BASE_URL2}/hmpc/gethmpcdata`)
+            .then((res) => {
+                console.log(res.data.data);
+                setRowData(res.data.data);
+                // setHm_pc_id(res.data.data.hm_pc_id);
+            })
+            .catch((err) => console.error(err));
+    }, []);
+
+
+    return (
+        <div className='main_container'>
+            <div className="logo_content">
+                <img
+                    src={logo_img}
+                    alt="logo"
+                />
+            </div>
+            {/* <div className="underline"></div> */}
+            <div className="title">
+                <p className="title_p">HM-PC Verification for CRM</p>
+            </div>
+            <div className="ag-theme-alpine" style={{ height: '100vh', width: '100%' }}>
+
+                {rowData && <AgGridReact
+                    rowData={rowData}
+                    columnDefs={columnDefs}
+                    defaultColDef={defaultColDef}
+                    theme="legacy"
+                    rowSelection="multiple"
+                    animateRows={true}
+                    pagination={true}
+                    paginationPageSize={50}
+                />}
+            </div>
+
+            {isVisibleModal && kycDetails === "HM" && (
+                <HM_Details_Modal
+                    toggleContainerHmPc={hideModal}
+                    hm_pc_data={hmpcData}
+                    hm_pc_id={hm_pc_id}
+                />
+            )}
+
+            {isVisibleModal && kycDetails === "PC" && (
+                <PC_Details_Modal
+                    toggleContainerHmPc={hideModal}
+                    hm_pc_data={hmpcData}
+                    hm_pc_id={hm_pc_id}
+                />
+            )}
+            {isVisibleModal && kycDetails === "Both Of Them" && (
+                <HM_PC_Modal
+                    toggleContainerHmPc={hideModal}
+                    hm_pc_data={hmpcData}
+                    hm_pc_id={hm_pc_id}
+                />
+            )}
+
+        </div>
+    );
+}
+
+export default CrmDashboard;
